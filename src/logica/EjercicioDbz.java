19fbd9ac9837137112a3f81ad783aee53117a579
fernando_fx5 <fernandonicolas91@hotmail.com>
2024-05-25 03:06:28 -0300

    Se agrego objeto Gohan al Main

--- src/logica/EjercicioDbz.java
+++ src/logica/EjercicioDbz.java
@@ -11,6 +11,11 @@ public class EjercicioDbz {
         Vegeta vegeta=new Vegeta(2,"Vegeta",62.1,"Masculino",3);
         System.out.println(vegeta.getNombre()+". " + "Soy el Principe ");
         vegeta.finalBlast();
+        
+        Gohan gohan=new Gohan(3,"Gohan",68.5,"Masculino",1);
+        gohan.setNombre("Gohan Definitivo");
+        System.out.println("Hola soy el hijo de Goku "+gohan.getNombre());
+        gohan.mazenko();
     }
     
 }
